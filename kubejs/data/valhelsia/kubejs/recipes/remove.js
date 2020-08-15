33f995db9e7ef23b862985ecd6eb0d82b59f8580
Khytwel <54046029+Khytwel@users.noreply.github.com>
2020-08-15 15:49:34 +0200

    Disabled Sleeping Bag from Comforts and Steel from IE.

--- kubejs/data/valhelsia/kubejs/recipes/remove.js
+++ kubejs/data/valhelsia/kubejs/recipes/remove.js
@@ -8,7 +8,7 @@ events.listen('recipes', function (event) {
     'decorative_blocks:chain',
 
     //Industrial Foregoing
-    'industrialforegoing:infinity_drill'
+    'industrialforegoing:infinity_drill',
   ]
   idRemove.forEach(function (remove) {
     event.remove({id: remove})
@@ -20,7 +20,38 @@ events.listen('recipes', function (event) {
     'immersiveengineering:ingot_copper',
     'immersiveengineering:nugget_copper',
     'immersiveengineering:ingot_lead',
-    'immersiveengineering:nugget_lead'
+    'immersiveengineering:nugget_lead',
+    'immersiveengineering:ingot_steel',
+    'immersiveengineering:nugget_steel',
+    'immersiveengineering:sword_steel',
+    'immersiveengineering:pickaxe_steel',
+    'immersiveengineering:shovel_steel',
+    'immersiveengineering:axe_steel',
+    'immersiveengineering:hoe_steel',
+
+    //Comforts
+    'comforts:sleeping_bag_white',
+    'comforts:sleeping_bag_orange',
+    'comforts:sleeping_bag_magenta',
+    'comforts:sleeping_bag_light_blue',
+    'comforts:sleeping_bag_yellow',
+    'comforts:sleeping_bag_lime',
+    'comforts:sleeping_bag_pink',
+    'comforts:sleeping_bag_gray',
+    'comforts:sleeping_bag_light_gray',
+    'comforts:sleeping_bag_cyan',
+    'comforts:sleeping_bag_purple',
+    'comforts:sleeping_bag_blue',
+    'comforts:sleeping_bag_brown',
+    'comforts:sleeping_bag_green',
+    'comforts:sleeping_bag_red',
+    'comforts:sleeping_bag_black',
+
+    //Mekanism Tools
+    'mekanismtools:steel_helmet',
+    'mekanismtools:steel_chestplate',
+    'mekanismtools:steel_leggings',
+    'mekanismtools:steel_boots'
   ]
   outputRemove.forEach(function (remove) {
     event.remove({output: remove})
