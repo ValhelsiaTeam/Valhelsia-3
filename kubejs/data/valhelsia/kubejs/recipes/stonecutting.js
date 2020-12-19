b9980568d742fe5796c8943302da5c887f3b59e3
Khytwel <54046029+Khytwel@users.noreply.github.com>
2020-12-19 09:35:59 +0100

    Add stonecutting unifaction with Create & Quark.

---
+++ kubejs/data/valhelsia/kubejs/recipes/stonecutting.js
@@ -0,0 +1,19 @@
+events.listen('recipes', function (event) {
+
+  //Quark
+  event.stonecutting(item.of('create:limestone', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:chiseled_limestone_bricks', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_bricks', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_pavement', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_pillar', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_slab', 2), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_speleothem', 2), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_stairs', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone_wall', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:polished_limestone', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:polished_limestone_slab', 2), 'quark:limestone')
+  event.stonecutting(item.of('quark:polished_limestone_stairs', 1), 'quark:limestone')
+
+  //Create
+  event.stonecutting(item.of('quark:limestone', 1), 'create:limestone')
+})
