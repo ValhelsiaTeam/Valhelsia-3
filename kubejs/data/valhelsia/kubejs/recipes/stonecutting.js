43f44556bfa192919a401eefbb6b84f971616111
Khytwel <54046029+Khytwel@users.noreply.github.com>
2020-12-19 10:50:28 +0100

    Add stonecutting unifaction with Astral Sorcery & Quark.

--- kubejs/data/valhelsia/kubejs/recipes/stonecutting.js
+++ kubejs/data/valhelsia/kubejs/recipes/stonecutting.js
@@ -13,7 +13,11 @@ events.listen('recipes', function (event) {
   event.stonecutting(item.of('quark:polished_limestone', 1), 'quark:limestone')
   event.stonecutting(item.of('quark:polished_limestone_slab', 2), 'quark:limestone')
   event.stonecutting(item.of('quark:polished_limestone_stairs', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:marble', 1), 'astralsorcery:marble_raw')
 
   //Create
   event.stonecutting(item.of('quark:limestone', 1), 'create:limestone')
+
+  //Astral Sorcery
+  event.stonecutting(item.of('astralsorcery:marble_raw', 1), 'quark:marble')
 })
