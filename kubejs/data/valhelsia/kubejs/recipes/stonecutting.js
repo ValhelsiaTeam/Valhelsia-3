07927ff37a0b5baad28f478bb5a118444bdac628
Vaelzan <vaelzan@evilcult.net>
2021-01-09 21:54:13 +1100

    Update stonecutting.js

--- kubejs/data/valhelsia/kubejs/recipes/stonecutting.js
+++ kubejs/data/valhelsia/kubejs/recipes/stonecutting.js
@@ -1,7 +1,6 @@
 events.listen('recipes', function (event) {
 
   //Quark
-  event.stonecutting(item.of('create:limestone', 1), 'quark:limestone')
   event.stonecutting(item.of('quark:chiseled_limestone_bricks', 1), 'quark:limestone')
   event.stonecutting(item.of('quark:limestone_bricks', 1), 'quark:limestone')
   event.stonecutting(item.of('quark:limestone_pavement', 1), 'quark:limestone')
@@ -13,10 +12,11 @@ events.listen('recipes', function (event) {
   event.stonecutting(item.of('quark:polished_limestone', 1), 'quark:limestone')
   event.stonecutting(item.of('quark:polished_limestone_slab', 2), 'quark:limestone')
   event.stonecutting(item.of('quark:polished_limestone_stairs', 1), 'quark:limestone')
+  event.stonecutting(item.of('quark:limestone', 1), 'create:limestone')
   event.stonecutting(item.of('quark:marble', 1), 'astralsorcery:marble_raw')
 
   //Create
-  event.stonecutting(item.of('quark:limestone', 1), 'create:limestone')
+  event.stonecutting(item.of('create:limestone', 1), 'quark:limestone')
 
   //Astral Sorcery
   event.stonecutting(item.of('astralsorcery:marble_raw', 1), 'quark:marble')
