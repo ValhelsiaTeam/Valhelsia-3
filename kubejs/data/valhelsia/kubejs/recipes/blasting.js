42131116ace97d16379c3a82895e89ed2a93b185
Vaelzan <vaelzan@evilcult.net>
2021-01-13 03:58:34 +1100

    Make XP amount given from smelting/blasting silver explicit.

--- kubejs/data/valhelsia/kubejs/recipes/blasting.js
+++ kubejs/data/valhelsia/kubejs/recipes/blasting.js
@@ -1,4 +1,4 @@
 events.listen('recipes', function (event) {
-  //Immersive Engineering
-  event.blasting(Item.of('immersiveengineering:ingot_silver', 1), '#forge:dusts/silver')
+  // Immersive Engineering
+  event.blasting(Item.of('immersiveengineering:ingot_silver', 1), '#forge:dusts/silver').xp(0.35)
 })
--- kubejs/data/valhelsia/kubejs/recipes/smelting.js
+++ kubejs/data/valhelsia/kubejs/recipes/smelting.js
@@ -1,4 +1,4 @@
 events.listen('recipes', function (event) {
-  //Immersive Engineering
-  event.smelting(Item.of('immersiveengineering:ingot_silver', 1), '#forge:dusts/silver')
+  // Immersive Engineering
+  event.smelting(Item.of('immersiveengineering:ingot_silver', 1), '#forge:dusts/silver').xp(0.35)
 })
