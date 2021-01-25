94c25e533cfb9c19ecf506ce9d1523380a01d2ed
Vaelzan <vaelzan@evilcult.net>
2021-01-26 10:21:18 +1100

    Fix Rocky Dirt processing, now always produces coarse dirt.

--- kubejs/server_scripts/recipes/create.js
+++ kubejs/server_scripts/recipes/create.js
@@ -9,6 +9,6 @@ events.listen('recipes', function (event) {
   }
 
   // Decorative Blocks
-  crushAndMill([Item.of('minecraft:dirt', 2), Item.of('minecraft:cobblestone', 2)], 'decorative_blocks:rocky_dirt')
+  crushAndMill('minecraft:coarse_dirt', 'decorative_blocks:rocky_dirt')
 
 })
--- kubejs/server_scripts/recipes/immersive_engineering.js
+++ kubejs/server_scripts/recipes/immersive_engineering.js
@@ -2,5 +2,5 @@
 
 events.listen('recipes', function (event) {
   // Decorative Blocks
-  event.recipes.immersiveengineering.crusher([Item.of('minecraft:dirt', 2), Item.of('minecraft:cobblestone', 2)], 'decorative_blocks:rocky_dirt')
+  event.recipes.immersiveengineering.crusher('minecraft:coarse_dirt', 'decorative_blocks:rocky_dirt')
 })
