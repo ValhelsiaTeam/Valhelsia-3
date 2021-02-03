bc49cc5864033b774dc56592464d24b163740f62
Vaelzan <vaelzan@evilcult.net>
2021-02-04 08:14:59 +1100

    Add additional squeezer and fermenter recipes.
    Compatibility with:
    Druidcraft Hemp Seeds -> Plant Oil
    Farmer's Delight Rice -> Ethanol
    Mystical World Aubergine Seeds -> Plant Oil
    
    And functions to easily add more in future.

--- kubejs/server_scripts/recipes/immersive_engineering.js
+++ kubejs/server_scripts/recipes/immersive_engineering.js
@@ -1,6 +1,32 @@
 // priority: 10
 
 events.listen('recipes', function (event) {
+
+  const fermenter = (fluid, input, amount) => {
+    event.custom({
+        type: 'immersiveengineering:fermenter',
+        fluid: {
+          fluid: fluid,
+          amount: amount
+        },
+        input: Ingredient.of(input).toJson(),
+        energy: 6400
+      
+    })
+  }
+
+  const squeezer = (fluid, input, amount) => {
+    event.custom({
+        type: 'immersiveengineering:squeezer',
+        fluid: {
+          fluid: fluid,
+          amount: amount
+        },
+        input: Ingredient.of(input).toJson(),
+        energy: 6400
+    })
+  }
+
   // Atmospheric Compat
   event.recipes.immersiveengineering.metal_press(Item.of('atmospheric:aloe_leaves', 9), 'atmospheric:aloe_bundle', 'immersiveengineering:mold_unpacking')
   event.recipes.immersiveengineering.metal_press(Item.of('atmospheric:barrel_cactus', 9), 'atmospheric:barrel_cactus_batch', 'immersiveengineering:mold_unpacking')
@@ -22,8 +48,15 @@ events.listen('recipes', function (event) {
   // Decorative Blocks Compat
   event.recipes.immersiveengineering.crusher('minecraft:coarse_dirt', 'decorative_blocks:rocky_dirt')
 
+  // Druidcraft Compat
+  squeezer('immersiveengineering:plantoil', 'druidcraft:hemp_seeds', 120)
+
+  // Farmer's Delight Compat
+  fermenter('immersiveengineering:ethanol', '#forge:seeds/rice', 80) // Also forward compatible with Environmental.
+
   // Forbidden & Arcanus Compat
   event.recipes.immersiveengineering.crusher(Item.of('forbidden_arcanus:soulless_sand', 2), '#forge:sandstone/soulless')
+  squeezer('immersiveengineering:plantoil', 'forbidden_arcanus:golden_orchid_seeds', 40)
 
   // Industrial Foregoing Compat
   event.recipes.immersiveengineering.fertilizer('industrialforegoing:fertilizer').growthModifier(1.5)
@@ -40,6 +73,9 @@ events.listen('recipes', function (event) {
   event.recipes.immersiveengineering.metal_press('minecraft:blaze_rod', Item.of('minecraft:blaze_powder', 6), 'immersiveengineering:mold_rod') 
   event.recipes.immersiveengineering.metal_press(Item.of('minecraft:dried_kelp', 9), 'minecraft:dried_kelp_block', 'immersiveengineering:mold_unpacking')
   event.recipes.immersiveengineering.metal_press(Item.of('minecraft:nether_wart', 4), 'minecraft:nether_wart_block', 'immersiveengineering:mold_unpacking')
+
+  // Mystical World Compat
+  squeezer('immersiveengineering:plantoil', '#forge:seeds/aubergine', 60)
 
   // Quark Compat
   event.recipes.immersiveengineering.crusher(Item.of('minecraft:soul_sand', 2), '#forge:sandstone/soul')
