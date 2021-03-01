c9461fc77af3f6e57899748e973bc5169d0f0897
Vaelzan <vaelzan@evilcult.net>
2021-03-01 12:45:42 +1100

    Disable Creeper Repellents in Neapolitan (Temp)
    To reduce lag - currently their creeper AI addition is very slow and frequently causes servers to take 5% more time to process a tick.

---
+++ kubejs/server_scripts/tags/block_tags_remove.js
@@ -0,0 +1,4 @@
+onEvent('block.tags', event => {
+  // Temporary change until Neapolitan improves their Creeper Repellent Code - causes a lot of lag currently.
+  event.removeAll('neapolitan:creeper_repellents')
+})
