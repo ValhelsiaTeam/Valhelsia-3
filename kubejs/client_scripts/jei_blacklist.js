991241c869877e25dbdcb406a346861d8963cd82
Vaelzan <vaelzan@evilcult.net>
2021-09-17 14:30:01 +1000

    Hide unused items from JEI.

--- kubejs/client_scripts/jei_blacklist.js
+++ kubejs/client_scripts/jei_blacklist.js
@@ -5,4 +5,8 @@ onEvent('jei.hide.items', event => {
   event.hide([
     /refinedstorage:(white|orange|magenta|yellow|lime|pink|gray|light_gray|cyan|purple|blue|brown|green|red|black)_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/
   ])
+
+  // Hide unused items.
+  event.hide('ftblibrary:fluid_container')
+
 })
