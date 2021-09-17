fca4f814dbd473d82b953c70889aee593a32f4fc
Vaelzan <vaelzan@evilcult.net>
2021-09-17 16:05:12 +1000

    Allow Moonstone to be crushed into Moonstone Shards.

--- kubejs/server_scripts/recipes/immersive_engineering.js
+++ kubejs/server_scripts/recipes/immersive_engineering.js
@@ -58,6 +58,7 @@ events.listen('recipes', function (event) {
 
   // Blue Skies Compat
   event.recipes.immersiveengineering.crusher(Item.of('minecraft:bone_meal', 9), 'blue_skies:azulfo_horn')
+  event.recipes.immersiveengineering.crusher(Item.of('blue_skies:moonstone_shard', 4), '#forge:gems/moonstone')
 
   // BoP Compat
   event.recipes.immersiveengineering.crusher('biomesoplenty:white_sand', 'quark:marble')
