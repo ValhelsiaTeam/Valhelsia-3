3c9f6aebcd90932a9cd6fba15d2fb8c1edbf3023
Vaelzan <vaelzan@evilcult.net>
2021-10-21 15:49:31 +1100

    Fix Title Screen, Paintings, World Generation, + remove redundant files + folders.

--- kubejs/client_scripts/jei_blacklist.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-
-onEvent('jei.hide.items', event => {
-  // Hide duplicate Refined Storage things that are just different colour variants. You can still see that they can be dyed by looking at the recipes / uses.
-  event.hide([
-    /refinedstorage:(white|orange|magenta|yellow|lime|pink|gray|light_gray|cyan|purple|blue|brown|green|red|black)_(controller|creative_controller|grid|crafting_grid|pattern_grid|fluid_grid|network_receiver|network_transmitter|relay|wireless_transmitter|disk_manipulator|crafter|crafter_manager|crafting_monitor|security_manager|detector)/
-  ])
-})
--- kubejs/server_scripts/tags/block_tags_remove.js
+++
@@ -1,5 +0,0 @@
-// priority: 90
-onEvent('block.tags', event => {
-  // Temporary change until Neapolitan improves their Creeper Repellent Code - causes a lot of lag currently.
-  event.removeAll('neapolitan:creeper_repellents')
-})
