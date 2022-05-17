9b5b3a4e60dca87f0b60c2a7536d717ee496f383
Vaelzan <vaelzan@evilcult.net>
2022-05-17 17:41:52 +1000

    Cleanup farmersdelight.js.

--- kubejs/server_scripts/recipes/farmersdelight.js
+++ kubejs/server_scripts/recipes/farmersdelight.js
@@ -12,69 +12,68 @@ onEvent('recipes', event => {
   // Cutting Board Axe Stripping
   const axeStripping = (output, input) => {
     event.custom({
-      type: "farmersdelight:cutting",
+      type: 'farmersdelight:cutting',
       ingredients: [
         Ingredient.of(input).toJson()
       ],
       tool: {
-        type: "farmersdelight:tool",
-        tool: "axe"
+        type: 'farmersdelight:tool',
+        tool: 'axe'
       },
       result: [
         Item.of(output).toResultJson(),
-        Item.of("farmersdelight:tree_bark").toResultJson()
+        {item: 'farmersdelight:tree_bark'}
       ],
-      sound: "minecraft:item.axe.strip"
-    })
-  }
+      sound: 'minecraft:item.axe.strip'
+    });
+  };
 
   // Cutting Board Axe Chopping
   const axeChopping = (output, input) => {
     event.custom({
-      type: "farmersdelight:cutting",
+      type: 'farmersdelight:cutting',
       ingredients: [
         Ingredient.of(input).toJson()
       ],
       tool: {
-        type: "farmersdelight:tool",
-        tool: "axe"
+        type: 'farmersdelight:tool',
+        tool: 'axe'
       },
       result: [
         Item.of(output).toResultJson()
       ]
-    })
-  }
+    });
+  };
 
   // Cutting Board Knife Cutting
   const knifeCutting = (output, input) => {
     event.custom({
-      type: "farmersdelight:cutting",
+      type: 'farmersdelight:cutting',
       ingredients: [
         Ingredient.of(input).toJson()
       ],
       tool: {
-        tag: "forge:tools/knives"
+        tag: 'forge:tools/knives'
       },
       result: output
-      
-    })
-  }
+    });
+  };
 
   // Cooking Pot Recipes
   const cookingPot = (output, input, container) => {
     event.custom({
-      type: "farmersdelight:cooking",
+      type: 'farmersdelight:cooking',
       ingredients: input,
       result: Item.of(output).toResultJson(),
       container: Item.of(container).toJson(),
       experience: 0.2,
       cookingtime: 200
-    })
-  }
+    });
+  };
 
   // ----- Axe Recipes -----
-  // BetterEnd - Log Stripping
-  const betterEndWoodTypes = [
+  // BetterEnd - Stripping + Chopping
+  [
     'dragon_tree',
     'end_lotus',
     'helix_tree',
@@ -85,19 +84,17 @@ onEvent('recipes', event => {
     'pythadendron',
     'tenanea',
     'umbrella_tree'
-  ]
+  ].forEach(function(element) {
+    axeStripping(`betterendforge:${element}_stripped_log`, `betterendforge:${element}_log`);
+    axeStripping(`betterendforge:${element}_stripped_bark`, `betterendforge:${element}_bark`);
 
-  betterEndWoodTypes.forEach(function(element) {
-    axeStripping(`betterendforge:${element}_stripped_log`, `betterendforge:${element}_log`)
-    axeStripping(`betterendforge:${element}_stripped_bark`, `betterendforge:${element}_bark`)
+    axeChopping(`betterendforge:${element}_planks`, `betterendforge:${element}_door`);
+    axeChopping(`betterendforge:${element}_planks`, `betterendforge:${element}_sign`);
+    axeChopping(`betterendforge:${element}_planks`, `betterendforge:${element}_trapdoor`);
+  });
 
-    axeChopping(`betterendforge:${element}_planks`, `betterendforge:${element}_door`)
-    axeChopping(`betterendforge:${element}_planks`, `betterendforge:${element}_sign`)
-    axeChopping(`betterendforge:${element}_planks`, `betterendforge:${element}_trapdoor`)
-  })
-
-  // Biomes 'o' Plenty - Log Stripping
-  const biomesOPlentyWoodTypes = [
+  // Biomes 'o' Plenty - Stripping + Chopping
+  [
     'fir',
     'redwood',
     'cherry',
@@ -109,61 +106,57 @@ onEvent('recipes', event => {
     'magic',
     'umbran',
     'hellbark'
-  ]
-
-  biomesOPlentyWoodTypes.forEach(function(element) {
-    axeStripping('biomesoplenty:stripped_'.concat(element).concat('_log'), 'biomesoplenty:'.concat(element).concat('_log'))
-    axeStripping('biomesoplenty:stripped_'.concat(element).concat('_wood'), 'biomesoplenty:'.concat(element).concat('_wood'))
+  ].forEach(function(element) {
+    axeStripping(`biomesoplenty:stripped_${element}_log`, `biomesoplenty:${element}_log`);
+    axeStripping(`biomesoplenty:stripped_${element}_wood`, `biomesoplenty:${element}_wood`);
     
-    axeChopping('biomesoplenty:'.concat(element).concat('_planks'), 'biomesoplenty:'.concat(element).concat('_door'))
-    axeChopping('biomesoplenty:'.concat(element).concat('_planks'), 'biomesoplenty:'.concat(element).concat('_trapdoor'))
-  })
+    axeChopping(`biomesoplenty:${element}_planks`, `biomesoplenty:${element}_door`);
+    axeChopping(`biomesoplenty:${element}_planks`, `biomesoplenty:${element}_trapdoor`);
+  });
   
   // Druidcraft - Log Stripping
-  axeStripping('druidcraft:stripped_darkwood_log', 'druidcraft:darkwood_log')
-  axeStripping('druidcraft:stripped_darkwood_wood', 'druidcraft:darkwood_wood')
-  axeStripping('druidcraft:stripped_elder_log', 'druidcraft:elder_log')
-  axeStripping('druidcraft:stripped_elder_wood', 'druidcraft:elder_wood')
+  axeStripping('druidcraft:stripped_darkwood_log', 'druidcraft:darkwood_log');
+  axeStripping('druidcraft:stripped_darkwood_wood', 'druidcraft:darkwood_wood');
+  axeStripping('druidcraft:stripped_elder_log', 'druidcraft:elder_log');
+  axeStripping('druidcraft:stripped_elder_wood', 'druidcraft:elder_wood');
 
   // Forbidden and Arcanus - Log Stripping
-  axeStripping('forbidden_arcanus:stripped_cherrywood_log', 'forbidden_arcanus:cherrywood_log')
-  axeStripping('forbidden_arcanus:stripped_cherrywood', 'forbidden_arcanus:cherrywood')
-  axeStripping('forbidden_arcanus:stripped_mysterywood_log', 'forbidden_arcanus:mysterywood_log')
-  axeStripping('forbidden_arcanus:stripped_mysterywood', 'forbidden_arcanus:mysterywood')
+  axeStripping('forbidden_arcanus:stripped_cherrywood_log', 'forbidden_arcanus:cherrywood_log');
+  axeStripping('forbidden_arcanus:stripped_cherrywood', 'forbidden_arcanus:cherrywood');
+  axeStripping('forbidden_arcanus:stripped_mysterywood_log', 'forbidden_arcanus:mysterywood_log');
+  axeStripping('forbidden_arcanus:stripped_mysterywood', 'forbidden_arcanus:mysterywood');
 
-  // Forbidden and Arcanus - Signs / Doors / Trapdoors -> Planks.
-  const choppingWoodTypes = [
+  // Forbidden and Arcanus - Chopping
+  [
     'forbidden_arcanus:edelwood',
     'forbidden_arcanus:cherrywood',
-    'forbidden_arcanus:mysterywood',
-  ]
-
-  choppingWoodTypes.forEach(function(element) {
-    axeChopping(element.concat('_planks'), element.concat('_sign'))
-    axeChopping(element.concat('_planks'), element.concat('_door'))
-    axeChopping(element.concat('_planks'), element.concat('_trapdoor'))
-  })
+    'forbidden_arcanus:mysterywood'
+  ].forEach(function(element) {
+    axeChopping(`${element}_planks`, `${element}_door`);
+    axeChopping(`${element}_planks`, `${element}_sign`);
+    axeChopping(`${element}_planks`, `${element}_trapdoor`);
+  });
 
   // ----- Knife Recipes -----
   // Aquaculture
-  knifeCutting([Item.of('aquaculture:frog_legs_raw'), Item.of('minecraft:bone_meal')], 'aquaculture:frog')
-  knifeCutting([Item.of('quark:frog_leg', 2)], 'aquaculture:frog_legs_raw')
-  knifeCutting([Item.of('quark:cooked_frog_leg', 2)], 'aquaculture:frog_legs_cooked')
+  knifeCutting([Item.of('aquaculture:frog_legs_raw'), Item.of('minecraft:bone_meal')], 'aquaculture:frog');
+  knifeCutting([Item.of('quark:frog_leg', 2)], 'aquaculture:frog_legs_raw');
+  knifeCutting([Item.of('quark:cooked_frog_leg', 2)], 'aquaculture:frog_legs_cooked');
 
   // Environmental
-  knifeCutting([Item.of('environmental:cattail_seeds'), Item.of('farmersdelight:straw')], 'biomesoplenty:cattail')
-  knifeCutting([Item.of('environmental:cattail_seeds'), Item.of('farmersdelight:straw')], 'environmental:cattail')
-  knifeCutting([Item.of('environmental:cattail_seeds', 2), Item.of('farmersdelight:straw')], 'environmental:tall_cattail')
+  knifeCutting([Item.of('environmental:cattail_seeds'), Item.of('farmersdelight:straw')], 'biomesoplenty:cattail');
+  knifeCutting([Item.of('environmental:cattail_seeds'), Item.of('farmersdelight:straw')], 'environmental:cattail');
+  knifeCutting([Item.of('environmental:cattail_seeds', 2), Item.of('farmersdelight:straw')], 'environmental:tall_cattail');
 
   // Farmer's Delight
-  knifeCutting([Item.of('farmersdelight:raw_pasta')], '#forge:food/dough')
+  knifeCutting([Item.of('farmersdelight:raw_pasta')], '#forge:food/dough');
 
   // Mystical World
-  knifeCutting([Item.of('mysticalworld:sliced_carrot', 4)], Ingredient.of('#forge:crops/carrot'))
+  knifeCutting([Item.of('mysticalworld:sliced_carrot', 4)], Ingredient.of('#forge:crops/carrot'));
 
   // ----- Cooking Pot Recipes -----
-  cookingPot('aquaculture:turtle_soup', [Ingredient.of('#aquaculture:turtle'), Ingredient.of('#forge:vegetables/onion')], 'minecraft:bowl')
-  cookingPot('forbidden_arcanus:bat_soup', [Ingredient.of('#forge:mushrooms'), Ingredient.of('#forge:crops/nether_wart'), Ingredient.of('forbidden_arcanus:bat_wing')], 'minecraft:bowl')
-  cookingPot('mysticalworld:stewed_eggplant', [Ingredient.of('#forge:crops/eggplant'), Ingredient.of('#forge:mushrooms'), Ingredient.of('#forge:vegetables/onion')], 'minecraft:bowl')
+  cookingPot('aquaculture:turtle_soup', [Ingredient.of('#aquaculture:turtle'), Ingredient.of('#forge:vegetables/onion')], 'minecraft:bowl');
+  cookingPot('forbidden_arcanus:bat_soup', [Ingredient.of('#forge:mushrooms'), Ingredient.of('#forge:crops/nether_wart'), Ingredient.of('forbidden_arcanus:bat_wing')], 'minecraft:bowl');
+  cookingPot('mysticalworld:stewed_eggplant', [Ingredient.of('#forge:crops/eggplant'), Ingredient.of('#forge:mushrooms'), Ingredient.of('#forge:vegetables/onion')], 'minecraft:bowl');
 
 });
